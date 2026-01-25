ecfc15d0b26866fb7746295fc154f24ec20ced7d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-24 20:57:39 -0500

    chore: p6df::modules::homebrew -> p6df::core::homebrew

--- init.zsh
+++ init.zsh
@@ -21,13 +21,13 @@ p6df::modules::sqlite::deps() {
 ######################################################################
 p6df::modules::sqlite::external::brew() {
 
-  p6df::modules::homebrew::cli::brew::install sqlite
-  p6df::modules::homebrew::cli::brew::install sqlite-analyzer
-  p6df::modules::homebrew::cli::brew::install sqlite-utils
-  p6df::modules::homebrew::cli::brew::install sqliteodbc
+  p6df::core::homebrew::cli::brew::install sqlite
+  p6df::core::homebrew::cli::brew::install sqlite-analyzer
+  p6df::core::homebrew::cli::brew::install sqlite-utils
+  p6df::core::homebrew::cli::brew::install sqliteodbc
 
-#  p6df::modules::homebrew::cli::brew::install --cask db-browser-for-sqlite
-#  p6df::modules::homebrew::cli::brew::install --cask sqlitemanager
+#  p6df::core::homebrew::cli::brew::install --cask db-browser-for-sqlite
+#  p6df::core::homebrew::cli::brew::install --cask sqlitemanager
 
   p6_return_void
 }
